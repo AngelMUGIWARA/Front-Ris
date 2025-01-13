32ca9e1d121d731a60cfa62e437f9875bb3bb647
Christian20233tn137 <20233tn137@utez.edu.mx>
2025-01-12 23:06:44 -0600

    pene

--- js/Turnos.js
+++ js/Turnos.js
@@ -1,18 +1,70 @@
-let turnoId = 2;
+document.addEventListener('DOMContentLoaded', function() {
 
-function agregarTurno() {
-    const tabla = document.getElementById('tabla-turnos');
-    const nuevaFila = document.createElement('tr');
-    nuevaFila.innerHTML = `
-        <td>${turnoId}</td>
-        <td>Nuevo Paciente</td>
-        <td>${new Date().toISOString().slice(0, 10)}</td>
-        <td><button class="btn btn-warning btn-sm" onclick="editarTurno(${turnoId})">Editar</button></td>
-    `;
-    tabla.appendChild(nuevaFila);
-    turnoId++;
-}
+    const API_URL = 'http://localhost:8080/turnos';
 
-function editarTurno(id) {
-    alert('Editar turno ' + id);
-}
+    function loadTable(){
+        fetch(API_URL, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            }
+        })
+        .then(response => response.json())
+        .then(data => {
+            const tablebody = document.querySelector('#tabla-turnos tbody');
+            tablebody.innerHTML = '';
+            data.results.forEach((turno, index) => {
+
+                //Columna de id
+                const row = document.createElement('tr');
+                const numberCell = document.createElement('th');
+                numberCell.scope = 'row';
+                numberCell.textContent = index + 1;
+                row.appendChild(numberCell);
+
+                //Columna de Paciente
+                const pacienteCell = document.createElement('td');
+                pacienteCell.textContent = turno.paciente;
+                row.appendChild(pacienteCell);
+
+                //Columna de Fecha
+                const fechaCell = document.createElement('td');
+                fechaCell.textContent = turno.fecha;
+                row.appendChild(fechaCell);
+
+                //Columnda Acciones
+                const actionscell = document.createElement('td');
+
+                //Boton Editar
+                const editButton = document.createElement('button');
+                editButton.className = 'btn btn-primary btn-sm mr-2';
+                editButton.textContent = 'Editar';
+                editButton.addEventListener('click', () => {
+                    openEditModal(turno.id, turno.paciente, turno.fecha);
+                });
+                actionscell.appendChild(editButton);
+
+                row.appendChild(actionscell);
+                tablebody.appendChild(row);
+            });
+
+        })
+        .catch(error => {
+            console.error('Error al obtener los datos:', error);
+        }); 
+    }
+
+    function openEditModal(id, paciente, fecha){
+        document.getElementById('turnoId').value = id;
+        document.getElementById('pacienteId').value = paciente;
+        document.getElementById('fechaId').value = fecha;
+        document.getElementById('editModal').style.display = 'block';
+    }
+
+    function closeEditModal() {
+        document.getElementById('categoryModal').style.display = 'none';
+    }
+
+    loadTable();
+});
